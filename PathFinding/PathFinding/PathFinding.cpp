679e571ba050814dc4dff8c4e6477f7a94075960
venage012 <venage012@gmail.com>
2026-06-16 15:26:48 +1200

    Finished check code, will need to check for the search code to make pathfinding work
    Yeah this is possible (Cope)

--- PathFinding/PathFinding/PathFinding.cpp
+++ PathFinding/PathFinding/PathFinding.cpp
@@ -31,6 +31,41 @@ PathFinding::~PathFinding()
 void PathFinding::CheckMap()
 {
 	bool Valid = true;
+	int StartTile = 0;
+	int ExitTile = 0;
+	int ItemTile = 0;
+
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			if (Map[i][j] == 'S')
+			{
+				StartTile += 1;
+			}
+			else if (Map[i][j] == 'X')
+			{
+				ExitTile += 1;
+			}
+			else if (Map[i][j] == 'a' || Map[i][j] == 'b' || Map[i][j] == 'c' || Map[i][j] == 'd' || Map[i][j] == 'e' || Map[i][j] == 'f' || Map[i][j] == 'g' || Map[i][j] == 'h' || Map[i][j] == 'i' || Map[i][j] == 'j')
+			{
+				ItemTile += 1;
+			}
+		}
+	}
+
+	if (StartTile < 1 || StartTile > 1)
+	{
+		Valid = false;
+	}
+	else if (ExitTile < 1 || ExitTile > 1)
+	{
+		Valid = false;
+	}
+	else if (ItemTile < 10 || ItemTile > 10)
+	{
+		Valid = false;
+	}
 
 	if (!Valid)
 	{
